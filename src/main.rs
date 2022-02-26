0246dc87db17a3c1594db034b47f94f730c107bf
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2022-02-26 22:29:24 +0100

    Add skeleton structure for maps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+mod map;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
---
+++ src/map.rs
@@ -0,0 +1,11 @@
+pub struct Node {
+}
+
+pub struct Map {
+}
+
+impl Map {
+    fn generate_random_map() -> Self {
+        Map {}
+    }
+}
