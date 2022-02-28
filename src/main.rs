00e3c680f04a263bc9cbaa6f2c2f961a5468fc9d
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2022-02-28 20:52:27 +0100

    Create macro for Bevy buttons
    Can now access settings page

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
 use dicey_dungeons::*;
 
 mod main_menu;
 mod game;
+mod settings;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum AppState {
     MainMenu,
     Game,
+    Settings,
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_state(AppState::MainMenu)
         .add_system_set(SystemSet::on_enter(AppState::MainMenu).with_system(main_menu::setup_menu))
         .add_system_set(SystemSet::on_update(AppState::MainMenu).with_system(main_menu::main_menu))
@@ -20,5 +24,6 @@ fn main() {
         .add_system_set(SystemSet::on_enter(AppState::Game).with_system(game::setup_game))
         .add_system_set(SystemSet::on_update(AppState::Game).with_system(game::update_game))
         .add_system_set(SystemSet::on_exit(AppState::Game).with_system(game::cleanup_game))
+        .add_system_set(SystemSet::on_update(AppState::Settings).with_system(settings::settings_ui))
         .run();
 }
--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,9 +1,10 @@
+use crate::AppState;
 use bevy::app::AppExit;
 use bevy::prelude::*;
-use crate::AppState;
 
 pub struct MainMenu {
     play_btn: Entity,
+    settings_btn: Entity,
     quit_btn: Entity,
 }
 
@@ -11,65 +12,49 @@ const NORMAL_BUTTON: Color = Color::rgb(0.35, 0.35, 0.35);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 
+macro_rules! button_with_text {
+    ($commands:ident, $assets:ident, $text:tt) => {
+        $commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                margin: Rect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    $text,
+                    TextStyle {
+                        color: Color::BLACK,
+                        font_size: 40.,
+                        font: $assets.load("fonts/FiraSans-Bold.ttf"),
+                        ..Default::default()
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        })
+        .id()
+    }
+}
+
 pub fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(UiCameraBundle::default());
-    let play_btn = commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                margin: Rect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..Default::default()
-            },
-            color: NORMAL_BUTTON.into(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Play",
-                    TextStyle {
-                        color: Color::BLACK,
-                        font_size: 40.,
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        ..Default::default()
-                    },
-                    Default::default()
-                ),
-                ..Default::default()
-            });
-        })
-        .id();
-    let quit_btn = commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                margin: Rect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..Default::default()
-            },
-            color: NORMAL_BUTTON.into(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Quit",
-                    TextStyle {
-                        color: Color::BLACK,
-                        font_size: 40.,
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        ..Default::default()
-                    },
-                    Default::default()
-                ),
-                ..Default::default()
-            });
-        })
-        .id();
-    commands.insert_resource(MainMenu { play_btn, quit_btn });
+    let play_btn = button_with_text!(commands, asset_server, "Play");
+    let settings_btn = button_with_text!(commands, asset_server, "Settings");
+    let quit_btn = button_with_text!(commands, asset_server, "Quit");
+    commands.insert_resource(MainMenu {
+        play_btn,
+        settings_btn,
+        quit_btn,
+    });
 }
 
 pub fn main_menu(
@@ -87,6 +72,8 @@ pub fn main_menu(
                 *color = PRESSED_BUTTON.into();
                 if entity == menu.play_btn {
                     state.set(AppState::Game).unwrap();
+                } else if entity == menu.settings_btn {
+                    state.set(AppState::Settings).unwrap();
                 } else {
                     app_exit_events.send(AppExit {});
                 }
@@ -103,5 +90,6 @@ pub fn main_menu(
 
 pub fn cleanup_menu(mut commands: Commands, menu: Res<MainMenu>) {
     commands.entity(menu.play_btn).despawn_recursive();
+    commands.entity(menu.settings_btn).despawn_recursive();
     commands.entity(menu.quit_btn).despawn_recursive();
 }
