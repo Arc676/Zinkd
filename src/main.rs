3407b52e3813ad2757f290a448808959c7143849
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2022-02-28 20:27:18 +0100

    Skeleton game state functions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use dicey_dungeons::*;
 
 mod main_menu;
+mod game;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum AppState {
@@ -16,5 +17,8 @@ fn main() {
         .add_system_set(SystemSet::on_enter(AppState::MainMenu).with_system(main_menu::setup_menu))
         .add_system_set(SystemSet::on_update(AppState::MainMenu).with_system(main_menu::main_menu))
         .add_system_set(SystemSet::on_exit(AppState::MainMenu).with_system(main_menu::cleanup_menu))
+        .add_system_set(SystemSet::on_enter(AppState::Game).with_system(game::setup_game))
+        .add_system_set(SystemSet::on_update(AppState::Game).with_system(game::update_game))
+        .add_system_set(SystemSet::on_exit(AppState::Game).with_system(game::cleanup_game))
         .run();
 }
